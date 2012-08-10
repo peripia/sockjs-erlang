433b154e36e7a0ce811369f74f039f722d6986a4
Ori Bar <ori.bar@nivertech.com>
2012-08-10 20:31:44 +0300

    more fixes to sockjs msg forwarding

--- src/sockjs_session.erl
+++ src/sockjs_session.erl
@@ -165,8 +165,10 @@ emit(What, State = #session{callback = Callback,
                 case What of
                     init            -> Callback:sockjs_init(Handle, UserState);
                     {recv, Data}    -> Callback:sockjs_handle(Handle, Data, UserState);
-                    {message, Data} -> Callback:sockjs_info(Handle, Data, UserState);
-                    tick            -> Callback:sockjs_tick(Handle, UserState);
+                    {message, Data} -> Callback:sockjs_info(Handle, What, UserState);
+                    {messages, Data}-> Callback:sockjs_info(Handle, What, UserState);
+                    tick            -> Callback:sockjs_info(Handle, What, UserState);
+                    force_disconnect-> Callback:sockjs_info(Handle, What, UserState);
                     closed          -> Callback:sockjs_terminate(Handle, UserState)
                 end
         end,
@@ -275,10 +277,13 @@ handle_cast({close, Status, Reason},  State = #session{response_pid = RPid}) ->
 handle_cast(Cast, State) ->
     {stop, {odd_cast, Cast}, State}.
 
-% Allow a handle_info-like call in sockjs handler. For now only messages of the form {message, X}, tick, and force_disconnect are supported
+% Allow a handle_info-like call in sockjs handler. For now only messages of the form {message, X}, {messages, X}, tick, and force_disconnect are supported
 % This is useful in cases where we have some main process sending message to processes corresponding to connections.
 handle_info({message, Raw}, State) -> 
     {noreply, emit({message, Raw}, State)};
+
+handle_info({messages, Raw}, State) -> 
+    {noreply, emit({messages, Raw}, State)};
 
 handle_info(tick, State) ->
     {noreply, emit(tick, State)};
