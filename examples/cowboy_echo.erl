033293385b31a1143e365628bb6817be2985d8f6
Michael Gebetsroither <michael@mgeb.org>
2013-04-08 12:23:50 +0200

    update cowboy_echo example to cowboy 0.8

--- examples/cowboy_echo.erl
+++ examples/cowboy_echo.erl
@@ -1,13 +1,12 @@
 #!/usr/bin/env escript
 %%! -smp disable +A1 +K true -pa ebin -env ERL_LIBS deps -input
-
 -module(cowboy_echo).
 -mode(compile).
 
 -export([main/1]).
 
 %% Cowboy callbacks
--export([init/3, handle/2, terminate/2]).
+-export([init/3, handle/2, terminate/3]).
 
 
 main(_) ->
@@ -20,14 +19,15 @@ main(_) ->
     SockjsState = sockjs_handler:init_state(
                     <<"/echo">>, fun service_echo/3, state, []),
 
-    VhostRoutes = [{[<<"echo">>, '...'], sockjs_cowboy_handler, SockjsState},
+    VhostRoutes = [{<<"/echo/[...]">>, sockjs_cowboy_handler, SockjsState},
                    {'_', ?MODULE, []}],
     Routes = [{'_',  VhostRoutes}], % any vhost
+    Dispatch = cowboy_router:compile(Routes),
 
     io:format(" [*] Running at http://localhost:~p~n", [Port]),
     cowboy:start_http(cowboy_echo_http_listener, 100, 
                       [{port, Port}],
-                      [{dispatch, Routes}]),
+                      [{env, [{dispatch, Dispatch}]}]),
     receive
         _ -> ok
     end.
@@ -43,12 +43,12 @@ handle(Req, State) ->
                                        Data, Req),
     {ok, Req1, State}.
 
-terminate(_Req, _State) ->
+terminate(_Reason, _Req, _State) ->
     ok.
 
 %% --------------------------------------------------------------------------
 
 service_echo(_Conn, init, state)        -> {ok, state};
 service_echo(Conn, {recv, Data}, state) -> Conn:send(Data);
-service_echo(Conn, {info, _Info}, state) -> {ok, state};
+service_echo(_Conn, {info, _Info}, state) -> {ok, state};
 service_echo(_Conn, closed, state)      -> {ok, state}.
